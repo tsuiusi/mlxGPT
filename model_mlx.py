4c59a65de57f0d793e7fbe973e63fd66f3b1775a
tsuiusi <ryantsui786@gmail.com>
2024-02-11 01:32:05 +0000

    mlp done

--- model_mlx.py
+++ model_mlx.py
@@ -97,7 +97,23 @@ class CausalSelfAttention(nn.Module):
         return y
 
 
-        
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4*config.n_embd, bias=config.bias) #4*config.n_embd because choice of design, bias is bool
+        self.gelu = nn.GELU()
+        self.c_proj = nn.Linear(4*config.n_embd, config.n_embd, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout) # probability
+    
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+
+        return x
+
+
 
 
 

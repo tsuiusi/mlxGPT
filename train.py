ef86375601324ff93bab20b349a7051ea0c6378c
tsuiusi <ryantsui786@gmail.com>
2024-04-13 14:32:42 +0100

    quicksave

--- train.py
+++ train.py
@@ -1,21 +1,3 @@
-"""
-This training script can be run both on a single gpu in debug mode,
-and also in a larger training run with distributed data parallel (ddp).
-
-To run on a single GPU, example:
-$ python train.py --batch_size=32 --compile=False
-
-To run with DDP on 4 gpus on 1 node, example:
-$ torchrun --standalone --nproc_per_node=4 train.py
-
-To run with DDP on 4 gpus across 2 nodes, example:
-- Run on the first (master) node with example IP 123.456.123.456:
-$ torchrun --nproc_per_node=8 --nnodes=2 --node_rank=0 --master_addr=123.456.123.456 --master_port=1234 train.py
-- Run on the worker node:
-$ torchrun --nproc_per_node=8 --nnodes=2 --node_rank=1 --master_addr=123.456.123.456 --master_port=1234 train.py
-(If your cluster does not have Infiniband interconnect prepend NCCL_IB_DISABLE=1)
-"""
-
 import os
 import time
 import math
@@ -29,3 +11,59 @@ import mlx.core as mx
 
 from model import GPTConfig, GPT
 
+"""
+1. Initialize hyperparameters
+2. Load data
+3. Load model
+4. Training loop
+5. Save weights
+"""
+
+# Hyperparameters
+# ------------------------------------------------------------------------------
+# Default values for GPT-2
+# I/O
+out_dir = 'out'
+eval_interval = 2000
+eval_iters = 200
+
+# Data
+dataset = 'openwebtext'
+gradient_accumulation_steps = 5 * 8
+batch_size = 12
+block_size = 1024
+
+# Model
+n_layer = 12
+n_head = 12
+n_embd = 768
+dropout = 0.0
+bias = False
+
+# AdamW
+lr = 6e-4
+max_iters = 600000
+weight_decay = 1e-1
+beta1 = 0.9
+beta2 = 0.95
+grad_clip = 1.0 # Clip gradient at this value
+
+# LR decay settings
+decay_lr = True
+warmup_iters = 2000
+lr_decay_iters = 600000
+min_lr = 6e-5 # Minimum learning rate 
+
+# Tokens
+tokens_per_iter = gradient_accumulation_steps * batch_size * block_size
+print(f"Tokens per iteration will be: {tokens_per_iter)")
+
+# ------------------------------------------------------------------------------ 
+# Poor man's data loader 
+data_dir = os.path.join('data', dataset)
+train_data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
+val_data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.r
